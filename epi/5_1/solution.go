c22f92862ddd82305e155c68fb08c7bd964af60b
Haiyan Meng <hmeng@nd.edu>
2016-04-21 20:34:21 -0400

    Add another solution to 5.1

--- epi/5_1/solution.go
+++ epi/5_1/solution.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// time complexity O(n), n is the word width in bits
 func ComputeParity(x uint64) (r bool) { // the (r bool) must be surrounded by a pair of parenthess
 	var i uint
 	for i=0; i<64; i++ {
@@ -18,20 +19,42 @@ func ComputeParity(x uint64) (r bool) { // the (r bool) must be surrounded by a 
 	return r
 }
 
-func main() {
+// time complexity is O(d), d is the number of the bits which are set to be 1
+func ComputeParity1(x uint64) (r bool) {
+	var c uint = 0
+	for x != 0 {
+		x &= (x-1)
+		c += 1
+	}
+
+	if c%2 == 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+type FuncTemp func(x uint64) (r bool)
+
+func test(f FuncTemp) {
 	var x uint64
 	x = 0x0b
-	fmt.Printf("the parity of %v(%b) is %v\n", x, x, ComputeParity(x))
+	fmt.Printf("the parity of %v(%b) is %v\n", x, x, f(x))
 	
 	x = 0x1b
-	fmt.Printf("the parity of %v(%b) is %v\n", x, x, ComputeParity(x))
+	fmt.Printf("the parity of %v(%b) is %v\n", x, x, f(x))
 
 	x = 0x3FFFFFFFFFFFFFFF
-	fmt.Printf("the parity of %v(%b) is %v\n", x, x, ComputeParity(x))
+	fmt.Printf("the parity of %v(%b) is %v\n", x, x, f(x))
 
 	x = 0x7FFFFFFFFFFFFFFF
-	fmt.Printf("the parity of %v(%b) is %v\n", x, x, ComputeParity(x))
+	fmt.Printf("the parity of %v(%b) is %v\n", x, x, f(x))
 
 	x = 0xFFFFFFFFFFFFFFFF
-	fmt.Printf("the parity of %v(%b) is %v\n", x, x, ComputeParity(x))
+	fmt.Printf("the parity of %v(%b) is %v\n", x, x, f(x))
+}
+
+func main() {
+	test(ComputeParity)
+	test(ComputeParity1)
 }
