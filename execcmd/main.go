bc0794d84ebd4134294906d500a6017aeac872ef
Haiyan Meng <hmeng@redhat.com>
2016-07-12 16:28:01 -0400

    Use variadic functions

--- execcmd/main.go
+++ execcmd/main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
-func execCmd(s []string) (string, error) {
-	if len(s) == 0 {
-		return "", errors.New("The command can not be empty!")
-	}
-
-	cmd := exec.Command(s[0], s[1:]...)
+func execCmd(name string, s ...string) (string, error) {
+	cmd := exec.Command(name, s...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
@@ -25,12 +22,31 @@ func execCmd(s []string) (string, error) {
 }
 
 func main() {
-	cmdSlice := []string{"runc", "-v"}
-
-	out, err := execCmd(cmdSlice)
+	out, err := execCmd("runc", "-v")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(out)
+
+	line := out[:strings.Index(out, "\n")]
+	fmt.Println(line)
+	v := line[strings.LastIndex(line, " ")+1:]
+	fmt.Println(v)
+	fmt.Println(len(v))
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cwd)
+	/*
+		cmdSlice = []string{
+			"bash",
+			"-c",
+			`grep -r "\"google.golang.org/grpc\"" /home/hmeng/go/src/github.com/hmeng-19/ocid/Godeps/Godeps.json -A 1 | grep "\"Rev\"" | cut -d: -f2 | tr -d ' "'`,
+		}
+
+	*/
 }
