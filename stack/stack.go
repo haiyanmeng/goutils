43954a4e1373ad04806a99e4eaf1e99b83e90337
Haiyan Meng <hmeng@nd.edu>
2016-08-22 14:15:25 -0400

    imp stack with max API

--- stack/stack.go
+++ stack/stack.go
@@ -30,3 +30,55 @@ func (s *Stack) Pop() (int, error) {
 func (s *Stack) Print() {
 	fmt.Printf("the stack is: %v\n", s.data)
 }
+
+type Elem struct {
+	data int
+	max  int
+}
+
+type MaxStack struct {
+	data []Elem
+	n    int
+}
+
+func NewMaxStack() *MaxStack {
+	return &MaxStack{}
+}
+
+func (s *MaxStack) Push(x int) {
+	var max int
+	if s.n == 0 {
+		max = x
+	} else {
+		max = s.data[s.n-1].max
+	}
+	if x > max {
+		max = x
+	}
+	s.data = append(s.data, Elem{x, max})
+	s.n++
+}
+
+func (s *MaxStack) Pop() (int, error) {
+	if s.n == 0 {
+		return 0, fmt.Errorf("the stack is empty")
+	}
+	x := s.data[s.n-1].data
+	s.data = s.data[:(s.n - 1)]
+	s.n--
+	return x, nil
+}
+
+func (s *MaxStack) Max() (int, error) {
+	if s.n == 0 {
+		return 0, fmt.Errorf("the stack is empty")
+	}
+	return s.data[s.n-1].max, nil
+}
+
+func (s *MaxStack) Print() {
+	for i := 0; i < s.n; i++ {
+		fmt.Printf("%v(max:%v) ", s.data[i].data, s.data[i].max)
+	}
+	fmt.Printf("\n")
+}
--- stack/test/main.go
+++ stack/test/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hmeng-19/goutils/stack"
 )
 
-func main() {
+func testStack() {
 	s := stack.NewStack()
 	for i := 0; i < 10; i++ {
 		s.Push(i)
@@ -26,4 +26,37 @@ func main() {
 		s.Push(i + 20)
 		s.Print()
 	}
+}
+
+func testMaxStack() {
+	s := stack.NewMaxStack()
+	for i := 0; i < 10; i++ {
+		s.Push(i)
+		s.Print()
+	}
+	for i := 0; i < 5; i++ {
+		x, err := s.Pop()
+		fmt.Printf("the popped item is %v\n", x)
+		s.Print()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		s.Push(30 - i)
+		s.Print()
+		max, err := s.Max()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("the max is %v\n", max)
+	}
+}
+
+func main() {
+	testStack()
+	testMaxStack()
 }
